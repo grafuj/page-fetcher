e998edcf344e37fa7c762e2b26f63dd1f7b28cb1
Graham Fujisawa <sfrrki@gmail.com>
2023-01-18 18:22:47 -0800

    fetcher now prints the website filesize if there is one

--- fetcher.js
+++ fetcher.js
@@ -1,15 +1,32 @@
 const fs = require('fs');
+// const filesize = require("filesize");
+const request = require('request');
+const url = process.argv[2];
+const localPath = process.argv[3];
 
+// const {size} = fs.statSync('http://www.example.edu');
 
-
-const content = 'Some content!';
-
-fs.writeFile('/Users/joe/test.txt', content, err => {
-  if (err) {
-    console.error(err);
+// console.log(size);
+request(url, (error, response, body) => {
+  // const {size} = fs.statSync('http://www.example.edu');
+  console.error('error:', error); // Print the error if one occurred
+  if (error) { //we only need the error, typically we get null which is falsey
+    //we can denote error if we want
   }
-  // file written successfully
-});
 
-//example message
-// Downloaded and saved 3261 bytes to ./index.html
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+
+  if (!body) {
+    console.log('body is not (!)');
+    return;
+  }
+  fs.writeFile(localPath, body, error => {
+    if (!error) {
+      // body.length here is returning the number of characters. one character is 8 bytes
+      console.log(`Downloaded and saved ${body.length} bytes to ./index.html`);
+    } else {
+      console.log('File failed to write');
+    }
+  });
+});
